fa4ff5f8e04145629f797c01ebdf9568a805c00c
pishivam009 <pishivam009@gmail.com>
2021-10-09 17:06:43 +0530

    Initial Commit

---
+++ src/main/java/com/piyush/lodgy/LodgyApplication.java
@@ -0,0 +1,13 @@
+package com.piyush.lodgy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LodgyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LodgyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piyush/lodgy/LodgyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piyush.lodgy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LodgyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
